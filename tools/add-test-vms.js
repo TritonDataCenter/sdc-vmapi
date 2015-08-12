83ff20bed40b465ec6e9f3506a6557dc029eee64
Julien Gilli <julien.gilli@joyent.com>
2015-08-12 13:11:41 -0700

    ZAPI-655: support "marker" for pagination on /vms
    Add new parameter "marker" to handle pagination via VM markers instead
    of offset.
    
    Using "marker" prevents any duplicate entry to be listed if new entries
    are added while paginating through a dataset. A marker is a JSON string
    that uniquely identifies a VM object. See the changes made to the
    documentation for more information.
    
    The documentation is updated to deprecate usage of "offset" in favor of
    "marker". It also documents that using both "offset" and "marker"
    parameters is invalid and results in a request error.
    
    This change also enforces a maximum limit of 1000 entries in the
    response when listing VMs. This is likely to break existing clients that
    do not use node-sdc-clients. As a consequence, this change also bumps
    the version number in package.json to 8.0.0.
    
    Finally, it also sorts by default on uuid descending, so that any
    ListVms request can be used as the first page of a series of paginated
    requests using marker={"uuid":"some-uuid"}.

---
+++ tools/add-test-vms.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2015, Joyent, Inc.
+ */
+
+/*
+ * This script is a tool to add test VM entries in Moray. It will create fake
+ * VMs with the value 'test--' for the 'alias' property.
+ * You can specify the trace log level, the number of VMs to create and the
+ * number of VMs created concurrently on the command line like following:
+ *
+ * $ LOG_LEVEL=trace node tools/add-test-vms.js [nb_test_vms_to_create]
+ * [concurrency]
+ */
+
+var path = require('path');
+var fs = require('fs');
+
+var libuuid = require('libuuid');
+var bunyan = require('bunyan');
+var restify = require('restify');
+var assert = require('assert-plus');
+
+var testCommon = require('../test/lib/vm');
+
+var MORAY = require('../lib/apis/moray');
+
+var DEFAULT_NB_TEST_VMS_TO_CREATE = 60;
+var DEFAULT_CONCURRENCY = 10;
+
+function loadConfig() {
+    var configPath = path.join(__dirname, '..', 'config.json');
+
+    if (!fs.existsSync(configPath)) {
+        console.error('Config file not found: ' + configPath +
+          ' does not exist. Aborting.');
+        process.exit(1);
+    }
+
+    var theConfig = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+    return theConfig;
+}
+
+var config = loadConfig();
+
+log = this.log = new bunyan({
+    name: 'add-test-vms',
+    level: process.env.LOG_LEVEL || config.logLevel || 'debug',
+    serializers: restify.bunyan.serializers
+});
+
+var moray = new MORAY(config.moray);
+
+moray.connect();
+moray.once('moray-ready', function () {
+    log.debug('Morary ready!');
+
+    var nbTestVmsToCreate = Number(process.argv[2]) ||
+        DEFAULT_NB_TEST_VMS_TO_CREATE;
+    log.debug('Number of test VMs to create:', nbTestVmsToCreate);
+    assert.number(nbTestVmsToCreate);
+
+    var concurrency = Number(process.argv[3]) || DEFAULT_CONCURRENCY;
+    log.debug('concurrency:', concurrency);
+    assert.number(concurrency);
+
+    testCommon.createTestVMs(nbTestVmsToCreate, moray, {
+        concurrency: concurrency,
+        log: log
+    }, {}, function allVmsCreated(err) {
+        if (err) {
+            log.error({err: err}, 'Error when creating test VMs');
+        } else {
+            log.info('All VMs created successfully');
+        }
+
+        log.debug('Closing moray connection');
+        moray.connection.close();
+    });
+});
