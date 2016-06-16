6fc5f1960308531834be2293e6b38b17e56e0ce0
Julien Gilli <julien.gilli@joyent.com>
2016-06-15 18:18:18 -0700

    ZAPI-666: refactor handling of curl HEAD requests

---
+++ test/vms.curl-head-requests.test.js
@@ -0,0 +1,138 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2016, Joyent, Inc.
+ */
+
+// This tests suite is mainly a regression tests suite for
+// https://smartos.org/bugview/ZAPI-220.
+
+var child_process = require('child_process');
+var http = require('http');
+
+var assert = require('assert-plus');
+var once = require('once');
+
+function curlHeadRequest(path, callback) {
+    assert.string(path, 'path');
+    assert.func(callback, 'callback');
+
+    var endpoint = 'http://localhost/' + path;
+    var CURL_CMD =
+        ['curl', '-sS', '-i', endpoint, '-X', 'HEAD'].join(' ');
+
+    child_process.exec(CURL_CMD, callback);
+}
+
+function nonCurlHeadRequest(path, callback) {
+    assert.string(path, 'path');
+    assert.func(callback, 'callback');
+
+    var callbackOnce = once(callback);
+
+    var req = http.request({
+        hostname: 'localhost',
+        path: path,
+        method: 'HEAD',
+        headers: {
+            'user-agent': 'foobar',
+            connection: 'keep-alive'
+        }
+    }, function onResponse(res) {
+        // destroy the socket explicitly now since the request was
+        // explicitly requesting to not destroy the socket by setting
+        // its connection header to 'keep-alive'.
+        req.abort();
+        callbackOnce(null, res.headers);
+    });
+
+    req.on('error', function onReqError(err) {
+        callbackOnce(err);
+    });
+
+    req.end();
+}
+
+function headerInCurlOutput(output, headerName) {
+    assert.string(output, 'output');
+    assert.string(headerName, 'headerName');
+
+    var lines = output.split(/\n/);
+    var headerValue;
+    var headerRegexp = new RegExp(headerName + ':\\s*(\\w+)');
+
+    lines.some(function matchHeader(line) {
+        var matches = line.match(headerRegexp);
+        if (matches != null) {
+            headerValue = matches[1];
+            return true;
+        }
+        return false;
+    });
+
+    return headerValue;
+}
+
+exports.curl_headvms_request = function (t) {
+    curlHeadRequest('/vms', function onHeadResponse(err, stdout, stderr) {
+        var contentLengthHeaderNotPresent =
+            headerInCurlOutput(stdout, 'Content-Length') == null;
+
+        var connectionCloseHeaderPresent =
+            headerInCurlOutput(stdout, 'Connection') === 'close';
+
+        t.ok(contentLengthHeaderNotPresent,
+            'content-length header must not be present in response');
+        t.ok(connectionCloseHeaderPresent,
+            'connection response header must be set to close');
+
+        t.done();
+    });
+};
+
+exports.curl_headvm_request = function (t) {
+    curlHeadRequest('/vm', function onHeadResponse(err, stdout, stderr) {
+        var contentLengthHeaderNotPresent =
+            headerInCurlOutput(stdout, 'Content-Length') == null;
+
+        var connectionCloseHeaderPresent =
+            headerInCurlOutput(stdout, 'Connection') === 'close';
+
+        t.ok(contentLengthHeaderNotPresent,
+            'content-length header must not be present in response');
+        t.ok(connectionCloseHeaderPresent,
+            'connection response header must be set to close');
+
+        t.done();
+    });
+};
+
+exports.non_curl_headvms_request = function (t) {
+    nonCurlHeadRequest('/vms', function onHeaders(err, headers) {
+        t.ifError(err, 'request should not result in an error');
+
+        t.ok(headers.hasOwnProperty('content-length'),
+            'response must have content-length header');
+        t.equal(headers.connection, 'keep-alive',
+            'connection response header must be set to keep-alive');
+
+        t.done();
+    });
+};
+
+exports.non_curl_headvm_request = function (t) {
+    nonCurlHeadRequest('/vm', function onHeaders(err, headers) {
+        t.ifError(err, 'request should not result in an error');
+
+        t.ok(headers.hasOwnProperty('content-length'),
+            'response must have content-length header');
+        t.equal(headers.connection, 'keep-alive',
+            'connection response header must be set to keep-alive');
+
+        t.done();
+    });
+};
